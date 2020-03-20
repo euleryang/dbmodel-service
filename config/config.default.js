4e8ab09fc8fda7843b4a5a9748d108fd03190a96
euleryang <14162376@qq.com>
2020-03-21 00:38:30 +0800

    Egg test passed

---
+++ config/config.default.js
@@ -0,0 +1,27 @@
+exports.keys = "dbmodelcookie";
+// 添加 view 配置
+exports.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.tpl': 'nunjucks',
+    },
+  };
+
+// 添加 news 的配置项
+exports.news = {
+  pageSize: 5,
+  serverUrl: 'https://hacker-news.firebaseio.com/v0',
+};
+
+// config/config.default.js
+// add middleware robot
+exports.middleware = [
+  'robot'
+];
+
+// robot's configurations
+exports.robot = {
+  ua: [
+    /Baiduspider/i,
+  ]
+};
