f3d191199b0d5ba6760098dad8a304bff8df9390
euleryang <14162376@qq.com>
2020-03-23 00:21:56 +0800

    egg plugin rsa256

--- config/plugin.js
+++ config/plugin.js
@@ -9,3 +9,9 @@ exports.jwt = {
   enable: true,
   package: "egg-jwt"
 };
+
+const path = require('path');
+exports.ua = {
+  enable: true,
+  path: path.join(__dirname, '../lib/plugin/egg-ua'),
+};
